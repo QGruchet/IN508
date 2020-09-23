3b8ca69dcb40a247864870bb964ac8b8dea05256
qgruchet <quentin.gruchet@ens.uvsq.fr>
2020-09-23 17:23:30 +0200

    add fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,6 +1,12 @@
 public class Fraction {
+    private int num;
+    private int denum;
+    Fraction(int num, int denum){
+        this.num = num;
+        this.denum = denum;
+    }
 
-    class toString{
-        
+    public String toString(){
+        return this.num + "/" + this.denum;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Fraction fac = new Fraction(4,5);
+        fac.toString();
+        System.out.println(fac);
     }
 }
