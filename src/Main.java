a0955cf1cc48907f55b7c701ff17a277ed560af0
qgruchet <quentin.gruchet@ens.uvsq.fr>
2020-09-29 19:00:55 +0200

    Update simpleFraction

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+      
         Fraction fac = new Fraction(10,5.8);
         fac.toString();
-        System.out.println(fac);
+        System.out.print(fac + " = ");
         double res = fac.cal();
         System.out.println(res);
     }
